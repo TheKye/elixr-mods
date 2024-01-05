49c136a7eec714722dd9d853249773e16b899bac
kye <kye-a@hotmail.com>
2024-01-05 14:04:59 +1100

    Fixed Missing dependency for Artistry

--- 10/EM Artistry/Eco.EM.Artistry/ArtistryBase.cs
+++ 10/EM Artistry/Eco.EM.Artistry/ArtistryBase.cs
@@ -26,6 +26,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Text;
+using Eco.Shared.Math;
 
 namespace Eco.EM.Artistry
 {
@@ -59,82 +60,5 @@ namespace Eco.EM.Artistry
         }
 
         public string GetCategory() => "Elixr Mods";
-    }
-}
-
-[Category("Hidden")]
-[Tag("Logging")]
-public abstract class AxeItem : MeleeWeaponItem, IInteractor, IHasInteractions, IController, IViewController, IHasUniversalID
-{
-    private static IDynamicValue caloriesBurn = new ConstantValue(0f);
-
-    private static IDynamicValue damage = new ConstantValue(100f);
-
-    private static IDynamicValue tier = new ConstantValue(0f);
-
-    private static IDynamicValue skilledRepairCost = new ConstantValue(1f);
-
-    public override Item RepairItem => Item.Get<StoneItem>();
-
-    public override int FullRepairAmount => 1;
-
-    public override IDynamicValue CaloriesBurn => caloriesBurn;
-
-    public override IDynamicValue Damage => damage;
-
-    public override IDynamicValue Tier => tier;
-
-    public override IDynamicValue SkilledRepairCost => skilledRepairCost;
-
-    public override Type ExperienceSkill => typeof(LoggingSkill);
-
-    public override ItemCategory ItemCategory => ItemCategory.Axe;
-
-    public override bool IsValidForInteraction(Item item)
-    {
-        return item is LogItem;
-    }
-
-    [Interaction(InteractionTrigger.LeftClick, null, InteractionModifier.None, null, 0f, 0f, ClientPredictedBlockAction.None, 0, false, TriBool.None, null, AccessType.FullAccess, (InteractionFlags)0, new string[] { "Choppable" }, DisallowedEnvVars = new string[] { "felled" }, AnimationDriven = true, InteractionDistance = 1.5f)]
-    [Interaction(InteractionTrigger.LeftClick, "Slice", InteractionModifier.None, new string[] { "slice" }, 0f, 0f, ClientPredictedBlockAction.None, 0, false, TriBool.None, "#FFFF00", AccessType.FullAccess, (InteractionFlags)0, new string[] { }, AnimationDriven = true, InteractionDistance = 1.5f)]
-    public bool Chop(Player player, InteractionTriggerInfo triggerInfo, InteractionTarget target)
-    { 
-        var Damage = new MultiDynamicValue(MultiDynamicOps.Sum, new ConstantValue(0.5f), CreateDamageValue(player.User.Skillset.GetSkill(typeof(LoggingSkill)).Level, typeof(LoggingSkill), typeof(LoggersLuckTalent)));
-        if (triggerInfo == InteractionTrigger.LeftClick)
-        {
-            if (target.IsBlock && this.TryCreateMultiblockContext(out var context, target, player, true, null, null, null, null, "Choppable"))
-            {
-                using (GameActionPack pack = new GameActionPack())
-                {
-                    if (target.IsBlock)
-                    {
-                        TreeDebris treeDebris = target.Block().Get<TreeDebris>();
-                        if (treeDebris != null)
-                        {
-                            foreach (KeyValuePair<Type, Eco.Shared.Math.Range> debrisResource in ((TreeSpecies)EcoSim.GetSpecies(treeDebris.Species)).DebrisResources)
-                            {
-                                pack.AddToInventory(player.User.Inventory, Item.Get(debrisResource.Key), debrisResource.Value.RandInt, player.User);
-                            }
-                        }
-                    }
-
-                    context.ActionDescription = GameActionDescription.DoStr("clean up tree debris", "cleaning up tree debris");
-                    context.ExperiencePerAction *= 0.1f;
-                    pack.DeleteBlock(context);
-                    return !pack.TryPerform(player.User).Failed;
-                }
-            }
-
-            TreeEntity treeEntity = target.NetObj as TreeEntity;
-            if (treeEntity != null)
-            {
-                float damageReceived;
-                GameActionPack pack2 = treeEntity.TryApplyDamage(new GameActionPack(), player, Damage, target, this, out damageReceived);
-                pack2.UseTool(this.CreateMultiblockContext(player, applyXPSkill: true, treeEntity.Position.XYZi()));
-                return pack2.TryPerform(player.User).Success;
-            }
-        }
-
-        return false;
     }
 }
